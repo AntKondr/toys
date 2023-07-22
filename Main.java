09595f7070d20d7d220e136a2449c05a2f6732df
Anton_Kondratev <elkondro@gmail.com>
2023-07-22 20:11:57 +0700

    skellet

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ package toys;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Toys shop");
+        Lototron lototron = new Lototron();
     }
 }
--- Toy.java
+++ Toy.java
@@ -1,10 +1,40 @@
 package toys;
 
 public class Toy {
-    int id;
-    String name;
-    int amt;
-    int prob;
+    private int id;
+    private String name;
+    private int amt;
+    private int prob;
 
-    public Toy() {}
+    public Toy(int id,
+               String name,
+               int prob) {
+        this.id = id;
+        this.name = name;
+        this.prob = prob;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAmt() {
+        return amt;
+    }
+
+    public void setAmt(int amt) {
+        this.amt = amt;
+    }
+
+    public void decrAmt() {
+        amt -= 1;
+    }
+
+    public int getProb() {
+        return prob;
+    }
 }
